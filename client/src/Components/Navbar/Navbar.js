76a59dda27afc6ed55c59240b0fe1d7431e93bbd
sarahimhof13 <se.imhof@gmail.com>
2023-03-07 17:01:24 +1300

    update nav

--- client/src/Components/Navbar/Navbar.js
+++ client/src/Components/Navbar/Navbar.js
@@ -1,17 +1,23 @@
-import { Icon, IconContainer, NavContainer, Logo } from "./Navbar.styled";
-import githubIcon from "../../assets/icon-github.svg"
-import linkedinIcon from "../../assets/icon-linkedin.svg"
+import { Icon, IconContainer, NavContainer, Logo, LogoContainer, LinksContainer } from "./Navbar.styled";
 
 const Navbar = () => {
     return ( 
         <NavContainer>
-            <Logo href="/">sarahimhof</Logo>
+            <LogoContainer>
+                <img src="/assets/logo.svg" alt="" />
+                <Logo href="/">sarahimhof</Logo>
+            </LogoContainer>
+            <LinksContainer>
+                <a href="#skills">Skills</a>
+                <a href="#projects">Projects</a>
+                <a href="#contact">Contact</a>
+            </LinksContainer>
             <IconContainer>
                 <a href="http://github.com/sarahimhof13" target="_blank" rel="noreferrer">
-                    <Icon src={githubIcon} />
+                    <Icon src="/assets/icon-github.svg" />
                 </a>
                 <a href="https://www.linkedin.com/in/sarah-imhof" target="_blank" rel="noreferrer">
-                    <Icon src={linkedinIcon}/>
+                    <Icon src="/assets/icon-linkedin.svg"/>
                 </a>
             </IconContainer>
         </NavContainer>
--- client/src/Components/Navbar/Navbar.styled.js
+++ client/src/Components/Navbar/Navbar.styled.js
@@ -1,22 +1,38 @@
 import styled from "styled-components";
 import { Device } from "../../Devices";
 
-export const NavContainer = styled.nav`
+export const NavContainer = styled.header`
     display: flex;
     justify-content: space-around;
     padding: 20px 0;
     flex-wrap: wrap;
+    background-color: var(--slate);
 
     @media ${Device.tablet} {
         flex-wrap: nowrap;
         justify-content: space-between;
-        padding: 30px 0;
+        padding: 30px;
     }
 
     @media ${Device.desktop} {
-        padding: 40px 0;
+        padding: 40px 165px;
     }
 
+`
+
+export const LogoContainer = styled.div`
+    display: flex;
+    gap: 5px;
+    align-items: center;
+    margin-bottom: 20px;
+
+    @media ${Device.tablet} {
+        margin-bottom: 0;
+    }
+
+    & img {
+        height: 22px;
+    }
 `
 
 export const Logo = styled.a`
@@ -26,11 +42,10 @@ export const Logo = styled.a`
     font-size: 1.5rem;
     width: 100%;
     text-align: center;
-    margin-bottom: 20px;
 
     &:hover {
-        color: var(--aquamarine);
-        transition: 0.75s;
+        color: var(--pink);
+        transition: 0.2s;
     }
 
     @media ${Device.tablet} {
@@ -40,10 +55,41 @@ export const Logo = styled.a`
     }
 `
 
+export const LinksContainer = styled.nav`
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    gap: 20px;
+    margin-bottom: 20px;
+    width: 100%;
+
+    @media ${Device.tablet} {
+        margin: 0;
+        justify-content: right;
+        margin-right: 20px;
+    }
+
+    @media ${Device.desktop} {
+        margin-right: 32px;
+        gap: 32px;
+    }
+
+    & a {
+        color: var(--white);
+        text-decoration: none;
+        text-transform: uppercase;
+
+        &:hover {
+            color: var(--pink);
+            transition: 0.2s;
+        }
+    }
+`
+
 export const IconContainer = styled.div`
     display: flex;
     align-items: center;
-    gap: 40px;
+    gap: 20px;
 
     @media ${Device.tablet} {
         gap: 32px;
