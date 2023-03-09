e0cd7d1d3f0150cc29e41ac7a6b265beb1dc5f87
sarahimhof13 <se.imhof@gmail.com>
2023-03-09 20:10:48 +1300

    netlify form v2

--- client/src/Components/Footer/Footer.js
+++ client/src/Components/Footer/Footer.js
@@ -4,22 +4,22 @@ import FooterNavbar from "../Navbar/FooterNav";
 
 
 const Footer = () => {
-    return ( 
+    return (
         <FooterContainer>
             <FooterText>
                 <h2 id="contact">Contact</h2>
                 <p>I would love to hear from you. Please fill in the form, and I’ll get back to you as soon as possible.</p>
             </FooterText>
-            <FooterForm method="post" netlify data-netlify="true" name="contactForm" onSubmit="submit">
-                <input type="hidden" name="contactForm" value="contactForm" form-name="contactForm"/>
-                <FooterInput placeholder="NAME" id="name" name="name" />
-                <FooterInput placeholder="EMAIL" id="email" name="email" type="email"/>
-                <FooterTextArea placeholder="MESSAGE"id="message" name="message" />
-                <FormButton name="Send Message" type="submit"/>
+            <FooterForm method="post" name="contact">
+                <input type="hidden" name="form-name" value="contact" />
+                <FooterInput placeholder="NAME" id="name" name="name" type="text" />
+                <FooterInput placeholder="EMAIL" id="email" name="email" type="email" />
+                <FooterTextArea placeholder="MESSAGE" id="message" name="message" />
+                <FormButton name="Send Message" type="submit" />
             </FooterForm>
-            <FooterNavbar/>
+            <FooterNavbar />
         </FooterContainer>
-     );
+    );
 }
- 
+
 export default Footer;
